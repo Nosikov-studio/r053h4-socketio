e813065b8601e9dabdd6aa69e88baa580f86dfa6
Nos <zvezdazanatih@gmail.com>
2025-07-11 13:25:41 +0700

    17

--- assets/main.js
+++ assets/main.js
@@ -1,1 +1,10 @@
-console.log('hello!!!!!!!!!!!!!!!!!')
+console.log('hello!!!!!!!!!!!!!!!!!');
+const socket = io();
+const messages = document.querySelector('.messages');
+const form = document.querySelector('.form');
+const input = document.querySelector('.input');
+const nameBlock = document.querySelector('.name');
+
+const userName = prompt('Ваше имя:');
+nameBlock.innerHTML = `${userName}`;
+
