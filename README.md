c08ed99ab6644d046957887b49374778eb013849
Nos <zvezdazanatih@gmail.com>
2025-07-11 09:41:16 +0700

    1

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # r053h4-socketio
+Проект "h4" изучаем тему socket.io по мотивам урока 
+"Разработка простого чата Express js / Socket.io за 30 минут"
+https://www.youtube.com/watch?v=M9js5vt15-A
+
---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+const http = require('http').createServer(app)
+const io = require('socket.io')(http)
+
+
+http.listen(3000, () => {
+    console.log('Сервер стартанул')
+})
